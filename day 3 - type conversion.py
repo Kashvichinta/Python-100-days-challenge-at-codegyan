da84165f48093556989e3585db8e8bcb99f45bdd
Kashvi Chinta <120377972+Kashvichinta@users.noreply.github.com>
2026-01-09 13:06:52 +0530

    Add files via upload

---
+++ day 3 - type conversion.py
@@ -0,0 +1,33 @@
+# ===============================
+# TYPE CONVERSION IN PYTHON
+# ===============================
+
+# int to float
+a = 10
+b = float(a)
+print("Int to Float:", b)
+
+# float to int
+c = 12.7
+d = int(c)
+print("Float to Int:", d)
+
+# int to string
+e = 25
+f = str(e)
+print("Int to String:", f)
+
+# string to int
+g = "100"
+h = int(g)
+print("String to Int:", h)
+
+# string to float
+i = "45.6"
+j = float(i)
+print("String to Float:", j)
+
+# float to string
+k = 9.8
+l = str(k)
+print("Float to String:", l)
