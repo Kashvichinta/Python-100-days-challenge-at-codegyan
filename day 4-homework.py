327f834d314a8b2c8eba0c33fd1559a33dc55ee9
Kashvi Chinta <120377972+Kashvichinta@users.noreply.github.com>
2026-01-10 11:46:02 +0530

    Update day 4-homework.py

--- day 4-homework.py
+++ day 4-homework.py
@@ -1,7 +1,8 @@
 a=int(input("Enter number:"))
-if a>-1 and a==0
+if a>-1 and a==0:
       print("Positive Number and it has Zero too")
 else:
     print("Negative Number")
 print("Program Executed")    
       
+
