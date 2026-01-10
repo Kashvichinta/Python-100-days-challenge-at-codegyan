26a8b18650959e3993e24a78bc455d8e1d92a176
Kashvi Chinta <120377972+Kashvichinta@users.noreply.github.com>
2026-01-10 12:29:51 +0530

    Assignment Solutions

---
+++ day 4 - Assignment.py
@@ -0,0 +1,52 @@
+#1.
+'''a=int(input("Enter a number: "))
+if a>-1:
+    print("Positive Number")
+else:
+    print("Negative Number")
+print("Program Executed")'''
+             
+
+#1.
+'''
+a=int(input("Enter a Number: "))
+if a==0:
+    print("it is zero")
+else:
+    print("Number is not zero")
+print("Program Executed")    '''
+#2
+
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+c=int(input("Enter third number:"))
+if a>=b and a>=c:
+      print("Biggest number is: ",a)
+elif b>=a and b>=c:
+    print("Biggest number is: ",b)
+else:
+    print("Biggest number is: ",c)
+print("Program Executed")
+
+    
+#3.
+'''a=int(input("Enter a number:"))
+if a%5==0:
+    print("Factor of 5")
+else:
+    print("Not a factor of 5")
+print("Program Executed")'''
+
+#4
+
+'''a=int(input("Enter a number:"))
+if a%3==0 and a%5==0:
+      print("Factor of 3 and 5")
+elif a%5==0:
+    print("Factor of 5")
+elif a%3==0:
+    print("Factor of 3")
+else:
+    print("Not a factor of 3 and 5")
+print("Program Executed") '''   
+      
