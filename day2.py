33d557f87bf68c99de39638fceffe7edaca379f8
Kashvi Chinta <120377972+Kashvichinta@users.noreply.github.com>
2026-01-10 12:31:18 +0530

    day2.py

--- day2.py
+++ day2.py
@@ -4,7 +4,7 @@ age = 45 #integer
 weight = 100.256 #float type
 list1 = [1,2,3,1,2,'hi',2.4,(10,20)]  #list of items
 tuple1 = (10,20,20,'Welcome', 'hello' ) #Tuple of items
-cards = {'A',2,3,4,5,6,7,8.9,10,'J','Q','K',2,3,5} #Pack of cards in set datatype
+cards = {'A',2,3,4,5,6,7,8,9,10,'J','Q','K',2,3,5} #Pack of cards in set datatype
 student_marks={'maths':90, "telugu" : 50, "science" :90} #dictionary datatype
 
 print("My name is: ",name)
@@ -14,6 +14,7 @@ print("The list of items are: ",list1)
 print("The tuple of items are: ",tuple1)
 print("The pack of cards in set datatype are: ",cards)
 print("The student marks are: ",student_marks)
+
 
 
 
