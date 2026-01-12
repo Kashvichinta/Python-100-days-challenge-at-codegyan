61605c534ec4391251c3363a531c43ad798085ae
Kashvi Chinta <120377972+Kashvichinta@users.noreply.github.com>
2026-01-12 16:59:45 +0530

    conditional statements, loops and range codes

---
+++ day-6 Nested Conditional statements and loops.py
@@ -0,0 +1,61 @@
+'''n=int(input())
+if n%2==0:
+    if n>-1:
+        print("Number is +ve even")
+    else:
+        print("Number is -ve even")
+else:
+    if n>-1:
+        print("Number is +ve odd")
+    else:
+        print("Number is -ve odd")
+print("Program Executed") '''
+
+
+#check which one is even number or odd number in the given list of numbers
+
+'''lst=[2,4,3,5,6,7,9]
+for num in lst:
+    if num%2==0:
+        print(num, "is even")
+    else:
+        print(num, "is odd")
+print("Program done")     '''  
+
+'''#check which one +ve number or -ve number in the given list of numbers
+lst=[2,4,-3,5,-6,-7,9]
+for num in lst:
+    if num>=0:
+        print(num, "is Positive")
+    else:
+        print(num, "is Negative")
+print("Program done")   '''     
+
+
+'''#print 1-20 numbers using range function
+
+for num in range(1,21,1):
+    print(num)'''
+
+
+#print even numbers  in between 0-20 using range function
+
+for num in range(0,21,2):
+    print(num, end=",")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
